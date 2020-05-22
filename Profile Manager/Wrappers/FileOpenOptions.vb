8c0758fa8a10a8dddee9e47c57ebca1cd757f5d1
mjordan <meloncholia@gmail.com>
2020-05-22 00:39:46 -0400

    Added file

---
+++ Profile Manager/Wrappers/FileOpenOptions.vb
@@ -0,0 +1,13 @@
+﻿Namespace Wrappers
+    Public Class FileOpenOptions
+        Inherits Wrappers.OptionWrapper
+
+        Private _names = ({"DefaultDesignView",
+                           "DefaultDesignViewIsAssociative",
+                           "DefaultLevelOfDetail",
+                           "SkipAllUnresolvedFilesInAssembly",
+                           "SkipAllUnresolvedFilesInDrawing",
+                           "SkipAllUnresolvedFilesInPart"})
+        
+    End Class
+End NameSpace
