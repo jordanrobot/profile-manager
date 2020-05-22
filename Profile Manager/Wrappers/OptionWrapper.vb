4c99f12ca8d81684974c5ed582211d564301ee1d
mjordan <meloncholia@gmail.com>
2020-05-22 00:24:59 -0400

    created a base class for option wrappers

---
+++ Profile Manager/Wrappers/OptionWrapper.vb
@@ -0,0 +1,20 @@
+﻿Namespace Wrappers
+    Public Class OptionWrapper
+        
+        Public Options As New Dictionary(Of String, Object)
+        Private _names As List(Of String)
+
+        Public Sub GetOptions(ByRef i As Object)
+            For Each name As String In _names
+                options.Add(name, CallByName (i, name, vbGet))
+            Next
+        End Sub
+
+        Public Sub SetOptions(ByRef i As Object)
+            For Each k In options.Keys
+                CallByName(i, k, CallType.Let, options(k))
+            Next
+        End Sub
+    End Class
+
+End Namespace
